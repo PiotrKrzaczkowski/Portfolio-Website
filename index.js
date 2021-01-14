450a1b1d4793bc9ea99950a87164f1943f63e24b
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-01-14 10:51:08 +0100

    added junctions to burgerBtn and menu links

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// TOGGLE BURGER MENU
+// HANDLE BURGER MENU BUTTON AND MENU LINKS
 const ul = document.querySelector(".mobile-list");
 const burgerBtn = document.querySelector(".burger-btn");
+const ulBtns = document.querySelectorAll(".mobile-menu a");
 let open = false;
+
+ulBtns.forEach((i) =>
+  i.addEventListener("click", () => {
+    if (open) {
+      ul.classList.remove("active");
+      burgerBtn.classList.remove("active");
+      open = false;
+    }
+  })
+);
 
 burgerBtn.addEventListener("click", () => {
   if (!open) {
