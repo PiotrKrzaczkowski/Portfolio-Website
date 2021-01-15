271faa2ffc69564cae0734b3bc7fdb366217ba3f
Piotr Krzaczkowski <slimshade93@wp.pl>
2021-01-15 13:46:25 +0100

    animation contact fix

--- index.js
+++ index.js
@@ -61,11 +61,11 @@ document.addEventListener("scroll", () => {
 window.addEventListener("scroll", reveal);
 
 function reveal() {
-  let reveals = document.querySelectorAll(".reveal");
+  const reveals = document.querySelectorAll(".reveal");
   for (let i = 0; i < reveals.length; i++) {
     let windowHeight = window.innerHeight;
     let revealTop = reveals[i].getBoundingClientRect().top;
-    let revealPoint = 50;
+    const revealPoint = 50;
     if (revealTop < windowHeight - revealPoint) {
       reveals[i].classList.add("slide");
     } else {
